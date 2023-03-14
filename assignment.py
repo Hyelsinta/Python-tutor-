48af475764fd1958ae9ffa01941f1c445b86694e
Tiffany Pureheart <126727956+Greatestguru007@users.noreply.github.com>
2023-03-14 22:37:57 +0100

    assignment.py

--- assignment.py
+++ assignment.py
@@ -1,17 +1,17 @@
 print("hello world")
 
-name_1 = input ("my name is bot ziri ")
+Print("my name is bot ziri ")
 
-identity = input("what is your name? ")
-print("hello, "+identity)
+identity = input("what is your name?: ")
+print("hello, " + identity)
 
-age = input("how old are you? ")
+age = input("how old are you?: ")
 print(age)
-home = input("where do you live" )
+home = input("where do you live?: " )
 print(home)
-print("thats a good location")
+print("that's a good location")
 
-question=input ("can i give you a little advice?")
+question=input ("can i give you a little advice?: ")
 
 print("faith without work is dead")
 
