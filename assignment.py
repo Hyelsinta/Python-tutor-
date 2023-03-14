6a602d39da5d10394f4c0064b7d70d442e479717
Hyelsinta <127156492+Hyelsinta@users.noreply.github.com>
2023-03-14 01:03:42 +0100

    Assignments

---
+++ assignment.py
@@ -0,0 +1,18 @@
+print("hello world")
+
+name_1 = input ("my name is bot ziri ")
+
+identity = input("what is your name? ")
+print("hello, "+identity)
+
+age = input("how old are you? ")
+print(age)
+home = input("where do you live" )
+print(home)
+print("thats a good location")
+
+question=input ("can i give you a little advice?")
+
+print("faith without work is dead")
+
+
